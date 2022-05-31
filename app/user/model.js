6ed30c1d614a9790fd92f5c4adb9331e3d303c25
Estera Nowak <esteranowak95@gmail.com>
2022-05-31 10:46:09 +0200

    deleted files

--- app/user/model.js
+++
@@ -1,9 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class UserModel extends Model {
-  @attr('string') username;
-  @attr('string') password;
-  @attr('string') email;
-  @attr('boolean', { defaultValue: false }) isDeleted;
-  @attr('boolean', { defaultValue: false }) isAdmin;
-}
