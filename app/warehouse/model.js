47ff259518f2be2a99cb3ba0dfa1f393128c8fb5
Estera Nowak <esteranowak95@gmail.com>
2022-06-27 17:55:16 +0200

    Creating model warehouse

---
+++ app/warehouse/model.js
@@ -0,0 +1,9 @@
+import Model, { attr, belongsTo } from '@ember-data/model';
+
+export default class TableModel extends Model {
+  @attr('string') name;
+  @attr('string') count;
+  @attr('string') target;
+
+  @belongsTo('user') owner;
+}
