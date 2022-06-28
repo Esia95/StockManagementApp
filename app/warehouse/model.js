606c9c37e477ed7f442c25dad121efb42c94bf4c
Estera Nowak <esteranowak95@gmail.com>
2022-06-28 18:21:33 +0200

    Deleting power select

--- app/warehouse/model.js
+++ app/warehouse/model.js
@@ -1,7 +1,7 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class WarehouseModel extends Model {
-  @attr('string') name;
+  @attr('string') articles;
   @attr('string') count;
   @attr('string') target;
 
