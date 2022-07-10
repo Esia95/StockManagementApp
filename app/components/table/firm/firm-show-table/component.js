e79b635c0abd731b35c0902a292e9de0e9228410
Estera Nowak <esteranowak95@gmail.com>
2022-07-10 11:46:28 +0200

    deleting modal

--- app/components/table/firm/firm-show-table/component.js
+++ app/components/table/firm/firm-show-table/component.js
@@ -1,44 +1,13 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 
 export default class TableFirmFirmShowTableComponent extends Component {
   @service session;
   @service store;
-  @tracked isShowModal = false;
-
-  get hasEmptyFields() {
-    return !this.note;
-  }
 
   @action
   onDeleted(table) {
     return table.destroyRecord();
-  }
-
-  @action
-  onShowModal() {
-    this.isShowModal = true;
-  }
-
-  @action
-  onHideModal() {
-    this.isShowModal = false;
-  }
-
-  @action
-  onNoteChange(event) {
-    this.note = event.target.value;
-  }
-
-  @action
-  async onSave() {
-    const note = {
-      note: this.note,
-    };
-    const noteModel = this.store.createRecord('table', note);
-    await noteModel.save();
-    this.isShowCreateModal = false;
   }
 }
