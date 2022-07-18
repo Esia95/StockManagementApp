2046bff47a07ff23da85b705719c94ec29f5cdc2
Estera Nowak <esteranowak95@gmail.com>
2022-07-18 12:48:32 +0200

    creating modal to warehouse

--- app/components/table/warehouse/show-table/component.js
+++ app/components/table/warehouse/show-table/component.js
@@ -1,9 +1,25 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+// import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class TableWarehouseShowTable extends Component {
+  @tracked isShowModal = false;
+  @tracked selectedOrderToEdit;
+
   @action
   async onDeleted(order) {
     await order.destroyRecord();
+  }
+
+  @action
+  onShowModal(order) {
+    this.selectedOrderToEdit = order;
+    this.isShowModal = true;
+  }
+
+  @action
+  onHideModal() {
+    this.isShowModal = false;
   }
 }
