474d37e8a03a3032cfb64032d998198d2c5b7d76
Estera Nowak <esteranowak95@gmail.com>
2022-07-19 16:00:25 +0200

    deleting

--- app/components/table/selleo/selleo-show-table/component.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class TableTableFormComponent extends Component {
-  @action
-  onDeleted(selleo) {
-    return selleo.destroyRecord();
-  }
-}
--- app/table/model.js
+++
@@ -1,9 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class TableModel extends Model {
-  @attr('string') articles;
-  @attr('string') count;
-  @attr('string') target;
-  @attr('string') unit;
-  @attr('string') note;
-}
