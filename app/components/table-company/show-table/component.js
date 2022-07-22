0543e589a916cfd05242c635c8e90d64c84c93e9
Estera Nowak <esteranowak95@gmail.com>
2022-07-22 18:45:37 +0200

    adding modal to order

--- app/components/table-company/show-table/component.js
+++ app/components/table-company/show-table/component.js
@@ -1,16 +1,17 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-// import { tracked } from '@glimmer/tracking';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 // import moment from 'moment';
 
 export default class TableCompanyShowTableComponent extends Component {
-  // @tracked date = '';
+  @tracked date = '';
   @service store;
+  @tracked isShowModal = false;
+  @tracked selectedOrderToEdit;
 
   @action
   async onDateOrdered(order) {
-    // let date = moment().format('DD-MM-YYYY');
     order.createdDate = new Date();
     await order.save();
   }
@@ -18,5 +19,16 @@ export default class TableCompanyShowTableComponent extends Component {
   @action
   async onDeleted(order) {
     await order.destroyRecord();
+  }
+
+  @action
+  onShowModal(order) {
+    this.selectedOrderToEdit = order;
+    this.isShowModal = true;
+  }
+
+  @action
+  onHideModal() {
+    this.isShowModal = false;
   }
 }
---
+++ app/components/table-company/show-table/modal/component.js
@@ -0,0 +1,28 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class TableCompanyShowTableModal extends Component {
+  @service store;
+
+  get shouldDisabledButton() {
+    return !this.args.order.hasDirtyAttributes;
+  }
+
+  @action
+  onPropertyChange(propName, { target: { value } }) {
+    this.args.order[propName] = value;
+  }
+
+  @action
+  async onSave() {
+    await this.args.order.save();
+    this.args.onClose();
+  }
+
+  @action
+  onClose() {
+    this.args.order.rollbackAttributes();
+    this.args.onClose();
+  }
+}
