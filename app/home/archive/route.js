ae2a0e5db08fc7bcb190a97c85d3648e2d45b80f
Estera <99197755+Esia95@users.noreply.github.com>
2022-08-04 14:25:28 +0200

    Update app/home/archive/route.js
    Co-authored-by: Adam Woźny <wozny1989@gmail.com>

--- app/home/archive/route.js
+++ app/home/archive/route.js
@@ -4,7 +4,7 @@ import { inject as service } from '@ember/service';
 export default class ArchiveRoute extends Route {
   @service store;
 
-  async model() {
-    return await this.store.findAll('order');
+  model() {
+    return this.store.findAll('order');
   }
 }
