23ab1587362a83d1ed2f280a7b69e5e939ad2480
Estera Nowak <esteranowak95@gmail.com>
2022-08-09 18:54:48 +0200

    fix: adding line to archive component

--- app/components/archive/component.js
+++ app/components/archive/component.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+
 export default class ArchiveComponent extends Component {
   get archivedOrders() {
     return this.args.orders.filter(({ isArchived }) => isArchived);
