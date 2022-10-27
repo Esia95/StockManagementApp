55721f5c92936e2bf1334b198f6fc920b16da34c
Estera Nowak <esteranowak95@gmail.com>
2022-10-27 12:34:27 +0200

    small changes

--- app/application/route.js
+++ app/application/route.js
@@ -46,7 +46,7 @@ export default class ApplicationRoute extends Route {
 
     const firmModel = this.store.createRecord('company', {
       id: 3,
-      name: 'COMPANY',
+      name: 'NA FIRMĘ',
     });
     await firmModel.save();
 
@@ -70,6 +70,20 @@ export default class ApplicationRoute extends Route {
       tableFields: ['articles', 'count', 'createdDate'],
     });
     await softkraftModel.save();
+
+    const lawFirmModel = this.store.createRecord('company', {
+      id: 7,
+      name: 'KANCELARIA',
+      tableFields: ['articles', 'count', 'createdDate'],
+    });
+    await lawFirmModel.save();
+
+    const flatsModel = this.store.createRecord('company', {
+      id: 8,
+      name: 'MIESZKANIA',
+      tableFields: ['articles', 'count', 'createdDate'],
+    });
+    await flatsModel.save();
 
     const order1 = this.store.createRecord('order', {
       article: 'Płyn',
@@ -130,5 +144,27 @@ export default class ApplicationRoute extends Route {
       createdDate: new Date('06-07-2022'),
     });
     await order6.save();
+
+    const order7 = this.store.createRecord('order', {
+      article: 'Płyn',
+      count: '4',
+      unit: 'op',
+      targets: 'HQ',
+      note: '',
+      createdDate: new Date('21-05-2022'),
+      company: lawFirmModel,
+    });
+    await order7.save();
+
+    const order8 = this.store.createRecord('order', {
+      article: 'Płyn',
+      count: '4',
+      unit: 'op',
+      targets: 'HQ',
+      note: '',
+      createdDate: new Date('21-05-2022'),
+      company: flatsModel,
+    });
+    await order8.save();
   }
 }
