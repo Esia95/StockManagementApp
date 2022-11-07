4a5a83195c5e149dd6a8803862175b06ce34c86b
Estera Nowak <esteranowak95@gmail.com>
2022-11-07 13:02:21 +0100

    changing shared input

--- app/components/shared/input/component.js
+++ app/components/shared/input/component.js
@@ -1,32 +1,9 @@
-// import Component from '@glimmer/component';
-// import { action } from '@ember/object';
-
-// export default class SharedInputComponenet extends Component {
-//   @action
-//   onChange({ target: { value } }) {
-//     this.args.onChange(value, ...arguments);
-//   }
-// }
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class SharedInputComponenet extends Component {
-  get type() {
-    return this.args.type || 'input';
-  }
-
-  get chceckInputType() {
-    return this.args.type || 'text';
-  }
-
-  get size() {
-    const { size } = this.args;
-    const isCorrectSize = ['sm', 'md', 'lg'].includes(size);
-    return isCorrectSize ? `form-control-${size}` : '';
-  }
-
   @action
-  onClick() {
-    this.args.onClick?.(...arguments);
+  onChange({ target: { value } }) {
+    this.args.onChange(value, ...arguments);
   }
 }
